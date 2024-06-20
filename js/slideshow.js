47b714337a1e19d4d005ff29a45d550be11ecf55
Chuka nnoli <143997599+Chukannoli@users.noreply.github.com>
2024-06-20 21:41:02 +0200

    adding comments

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,6 +5,9 @@ let slideIndex = 0;
 let lastSlide = slides.length - 1;
 
 previousButton.addEventListener("click", previousSlide);
+nextButton.addEventListener("click", nextSlide);
+
+// Keyboard navigation
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") {
     previousSlide();
@@ -14,8 +17,8 @@ document.addEventListener("keydown", (e) => {
     return;
   }
 });
-nextButton.addEventListener("click", nextSlide);
 
+// Initializing the first slide after DOM content is loaded
 const initializeSlide = () => {
   if (slides.length > 0) {
     document.addEventListener("DOMContentLoaded", showSlide(slideIndex));
@@ -23,14 +26,14 @@ const initializeSlide = () => {
 };
 initializeSlide();
 
+// Showing curent slide
 function showSlide(index) {
   slides.forEach((slide) => {
     slide.classList.remove("active");
   });
-
   slides[index].classList.add("active");
 }
-
+// Previous slide function
 function previousSlide() {
   if (slideIndex <= 0) {
     slideIndex = lastSlide;
@@ -39,7 +42,7 @@ function previousSlide() {
   }
   showSlide(slideIndex);
 }
-
+// Next slide function
 function nextSlide() {
   if (slideIndex >= lastSlide) {
     slideIndex = 0;
