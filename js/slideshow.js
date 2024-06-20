20ccb0e3d5b6783b5b0e0b08db37be86e4ae168b
Chuka nnoli <143997599+Chukannoli@users.noreply.github.com>
2024-06-20 13:54:18 +0200

    initializing slides

--- js/slideshow.js
+++ js/slideshow.js
@@ -4,6 +4,15 @@ const nextButton = document.querySelector(".right");
 let slideIndex = 0;
 let lastSlide = slides.length - 1;
 
+console.log(slides);
+
+const initializeSlide = () => {
+  if (slides.length > 0) {
+    document.addEventListener("DOMContentLoaded", showSlide(slideIndex));
+  }
+};
+initializeSlide();
+
 function showSlide(index) {
   slides.forEach((slide) => {
     slide.classList.remove("active");
